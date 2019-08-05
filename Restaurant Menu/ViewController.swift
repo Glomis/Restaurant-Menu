6a1e813ae9776e0d82a9a909fe4bf453d08d7b7f
MAx <max@max.ru>
2019-08-05 21:41:25 +0300

    Initial Commit

---
+++ Restaurant Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant Menu
+//
+//  Created by Max on 05/08/2019.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
